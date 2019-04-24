d634595082b4eed1722695835c6caa40abcf717b
Aamir Mahmood <aamir@aalasolutions.com>
2019-04-24 12:57:36 +0500

    edit inventory completed

--- app/components/inventory/list-page.js
+++ app/components/inventory/list-page.js
@@ -2,6 +2,7 @@ import Component                         from '@ember/component';
 import EmberObject, {set, get, computed} from '@ember/object';
 import {inject as service}               from '@ember/service';
 import {later}                           from '@ember/runloop';
+import moment                            from 'moment';
 
 export default Component.extend({
   store     : service(),
@@ -12,18 +13,28 @@ export default Component.extend({
   newInventoryItem: null,
   init() {
     this._super(...arguments);
-    set(this, 'invent', this.store.peekAll('inventory'));
+    let medicine = this.store.peekRecord('medicine', get(this, 'model.id'));
+
+    set(this, 'invent', get(medicine, 'inventory'));
     set(this, 'inventSortProps', ['expiry:asc']);
-    set(this, 'medicineModel', this.store.peekRecord('medicine', this.get('model.id')));
+    set(this, 'medicineModel', this.store.peekRecord('medicine', get(this, 'model.id')));
     this.makeNewObj();
   },
 
+  didUpdateAttrs(){
+    if(get(this,'addNewInvent')){
+      set(this, 'editItem', null);
+      set(this, 'editId', null);
+      this.makeNewObj();
+    }
+
+  },
   makeNewObj() {
     let newItem = EmberObject.create({
       medicine: get(this, 'medicineModel'),
-      quantity: 0,
+      quantity: null,
       notes   : "",
-      expiry  : new Date(),
+      expiry  : moment(new Date()).add(2, 'months').format("YYYY-MM-DD"),
       created : new Date(),
     });
 
@@ -38,7 +49,7 @@ export default Component.extend({
       newInventory.set('medicine', inventoryData.get('medicine'));
       newInventory.set('quantity', inventoryData.get('quantity'));
       newInventory.set('notes', inventoryData.get('notes'));
-      newInventory.set('expiry', inventoryData.get('expiry'));
+      newInventory.set('expiry', moment(inventoryData.get('expiry')).toDate());
       newInventory.set('created', inventoryData.get('created'));
       newInventory.save().then(() => {
         this.toggleProperty('addNewInvent');
@@ -55,41 +66,42 @@ export default Component.extend({
     },
 
     editInventory(inventory) {
+      this.makeNewObj();
       let inventoryData = get(this, 'newInventoryItem');
 
-      set(this, 'editId', inventory.id);
-      set(this, 'editItem', inventory);
+      set(inventoryData, 'quantity', get(inventory,'quantity'));
+      set(inventoryData, 'notes', get(inventory,'notes'));
+      set(inventoryData, 'expiry', moment(get(inventory,'expiry')).format("YYYY-MM-DD"));
 
+      set(this, 'editItem', inventoryData);
+      set(this, 'editId', get(inventory, 'id'));
+      set(this, 'addNewInvent', false);
       later(this, () => {
         this.$('.edit_category_input input').focus();
       }, 500);
-
     },
 
-    editInventoryComplete(inventory) {
-      inventory.save();
-      set(this, 'editId', 0);
-      // this.get('message').success("Inventory Updated");
-      window.plugins.toast.show('Inventory Updated', 1500, 'bottom');
+
+    editInventorySave() {
+
+      let newInventory = this.store.peekRecord('inventory', get(this, 'editId'));
+
+      let inventoryData = get(this, 'editItem');
+      newInventory.set('quantity', inventoryData.get('quantity'));
+      newInventory.set('notes', inventoryData.get('notes'));
+      newInventory.set('expiry', moment(inventoryData.get('expiry')).toDate());
+      newInventory.save().then(() => {
+        set(this, 'editId', null);
+        set(this, 'editItem', null);
+        window.plugins.toast.show('Inventory Updated', 1500, 'bottom');
+
+        this.makeNewObj();
+      });
 
     },
+    editInventoryCancel() {
+      this.makeNewObj();
 
-    editInventorySave(inventory) {
-
-      set(inventory, 'category', this.store.peekRecord('category', get(this, 'newCatId')));
-
-      inventory.save();
-      set(this, 'editId', null);
-      set(this, 'editItem', null);
-      set(this, 'newCatId', null);
-
-
-      window.plugins.toast.show('Inventory Updated', 1500, 'bottom');
-
-
-    },
-    editInventoryCancel(inventory) {
-      inventory.rollbackAttributes();
       set(this, 'editId', null);
       set(this, 'editItem', null);
     },
